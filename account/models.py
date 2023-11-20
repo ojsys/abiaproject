9aaf2e8ae2c9484ce61156d863769dc655d6a6c3
Onah Jonah <onahjonah@gmail.com>
2023-11-20 21:57:01 +0100

    made changes to account models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,38 @@
+from typing import Any
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Please provide an email address')
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be a staff')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must setting is_superuser set to True')
+        
+        return self.create_user(email, password, **extra_fields)
+

fcb4957d72307408c243d775192e48bdd18af547
Onah Jonah <onahjonah@gmail.com>
2023-11-21 07:53:40 +0100

    changed made to settings

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 
 CustomUser = get_user_model()
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
